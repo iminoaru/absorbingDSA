5601f2e9006829c4c487aab0d6870184934a7a52
Sarthak Gaud <115076922+iminoaru@users.noreply.github.com>
2023-07-03 11:55:17 +0530

    Create nth-fibNo.java

--- Recursion/Basics/nth-fibNo.java
+++ Recursion/Basics/nth-fibNo.java
@@ -1,15 +1,19 @@
+// Example of multiple recursive calls.
+//	int last = fib(nth - 1);
+//	int secondlast = fib(nth - 2);
+//	return last + secondlast;
 
 public class Main{
-    
     static int fib(int nth){
         if(nth == 1) return 1;
         if(nth == 0) return 0;
         return fib(nth - 1) + fib(nth - 2);
     }
     
-	public static void main(String[] args){
-	    int nth = 15;
-		System.out.println(fib(nth));
-	}
+    public static void main(String[] args){
+	int nth = 15;
+	System.out.println(fib(nth));
+    }
 }
+
 // Time complixity is O(2^n) exponential time because each fn call calls 2 more fns intil nth reaches 0 or 1 
