8b42c23d7480949b9ad6013f30ff18a16a2ffa38
Sarthak Gaud <115076922+iminoaru@users.noreply.github.com>
2023-07-07 18:39:55 +0530

    Create Number-of-subsequences.java

---
+++ Recursion/Subsequences/Number-of-subsequences.java
@@ -0,0 +1,31 @@
+// Time O(2^n)      |       Space O(n)
+
+// The general formula to find number of subsequences is just 2^length of array.
+// This is a pattern that can also be used if some conditions are given like sum = k etc.
+
+import java.util.ArrayList;
+
+class Main{
+    static int findingSubsequence(int index , int[] arr){
+        int len = arr.length;
+        int left = 0 , right = 0;
+        
+        if(index == len) return 1; 
+
+        // PICK
+        left = findingSubsequence(index + 1 , arr);
+        
+        // NOT PICK
+        right = findingSubsequence(index + 1 , arr); 
+        
+        return left + right;
+    }
+    
+    public static void main(String[] args){
+        int[] arr = {3 , 2 , 5 , 3 , 6};
+       
+        int totalSubsequences = findingSubsequence(0 , arr);
+        System.out.println(totalSubsequences);
+        
+    }
+}
