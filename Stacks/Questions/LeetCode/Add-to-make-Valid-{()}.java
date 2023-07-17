90a178ae008029cc0b0d4ac87b39ccb170c46687
Sarthak Gaud <115076922+iminoaru@users.noreply.github.com>
2023-07-17 14:19:18 +0530

    Update Add-to-make-Valid-{()}.java

--- Stacks/Questions/LeetCode/Add-to-make-Valid-{()}.java
+++ Stacks/Questions/LeetCode/Add-to-make-Valid-{()}.java
@@ -1,1 +1,22 @@
+https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/submissions/
 
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()){ // iterates over every character of the string 's'
+            if(ch == ')'){
+                if(!stack.isEmpty() && stack.peek() == '('){
+                    stack.pop();
+                }
+                else{
+                    stack.push(ch);
+                }
+            }
+            else{
+                stack.push(ch);
+            }      
+        } 
+        return stack.size(); // number of parentheses left in stack are the number of parantheses that are not closed.
+    }
+}
