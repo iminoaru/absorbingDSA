224ba7fa3290d417e2c912567e8690ce1c0358c9
Sarthak Gaud <115076922+iminoaru@users.noreply.github.com>
2023-07-18 11:57:21 +0530

    Update Inorder.java

--- Binary Tree/Traversals/Iterative/Inorder.java
+++ Binary Tree/Traversals/Iterative/Inorder.java
@@ -1,1 +1,46 @@
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ //PreOrder traversal = left -> Root -> right
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+      
+        List<Integer> traversalPath = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        if(root == null) return traversalPath;
+
+        TreeNode node = root;
+        while(true){
+            if(node != null){
+                stack.push(node);
+                node = node.left;
+            }
+            else{
+                if(stack.isEmpty()) break; // exit condition of loop/
+                
+                node = stack.pop();
+                traversalPath.add(node.val);
+                node = node.right;
+            }
+        }
+
+     return traversalPath;
+
+    }
+}    
