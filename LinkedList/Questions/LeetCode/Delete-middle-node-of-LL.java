3d073b31aea87d8efa80f7e40e1f6e0fd7edf926
Sarthak Gaud <115076922+iminoaru@users.noreply.github.com>
2023-10-07 23:38:32 +0530

    added new solution of Delete-middle-node-of-LL.java

--- LinkedList/Questions/LeetCode/Delete-middle-node-of-LL.java
+++ LinkedList/Questions/LeetCode/Delete-middle-node-of-LL.java
@@ -10,33 +10,52 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-
-        if(head.next == null) return null;
-        
-        ListNode main = head;
-
-        ListNode temp = head;
-        int len = 0;
-
-        while(temp.next != null){
-            len++;
-            temp = temp.next;
+        if(head.next == null){
+            head = null;
+            return head;
         }
-        if(len%2==0){
-            for(int i = 1 ; i < len/2 ; i++){
-                main = main.next;
-            }
-        }    
+        ListNode slow = head;
+        ListNode fast = head.next.next;
 
-        else{
-            for(int i = 0 ; i < len/2 ; i++){
-                main = main.next;
-            }
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        main.next = main.next.next;
+        slow.next = slow.next.next;
+        //System.gc(); drastically increases the space complexity but reduces the time complexity by 4x
 
         return head;
+
+        
+        // if(head.next == null) return null;
+        
+        // ListNode main = head;
+
+        // ListNode temp = head;
+        // int len = 0;
+
+        // while(temp.next != null){
+        //     len++;
+        //     temp = temp.next;
+        // }
+        // if(len%2==0){
+        //     for(int i = 1 ; i < len/2 ; i++){
+        //         main = main.next;
+        //     }
+        // }    
+
+        // else{
+        //     for(int i = 0 ; i < len/2 ; i++){
+        //         main = main.next;
+        //     }
+        // }
+
+        // main.next = main.next.next;
+
+        // return head;
+
+        
 
     }
 }
